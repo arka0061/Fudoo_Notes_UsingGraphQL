33f637ed747e17a962f66f06477363614dbe8f62
Arka <arka.spectacular@gmail.com>
2021-09-22 23:14:24 +0530

    feat:.forget password api added

--- Fundoo_Notes_UsingGraphQL/app/graphql/resolvers/index.js
+++ Fundoo_Notes_UsingGraphQL/app/graphql/resolvers/index.js
@@ -1,9 +1,13 @@
 const createUser = require('./createUser');
 const users=require('./getUsers');
 const loginUser=require('./loginUsers');
+const forgotpassword=require('./forgotpassword');
+const resetpassword=require('./resetpassword');
 const rootResolver = {
   ...createUser,
   ...loginUser,
-  ...users
+  ...users,
+  ...forgotpassword,
+  ...resetpassword
 };
 module.exports = rootResolver;
