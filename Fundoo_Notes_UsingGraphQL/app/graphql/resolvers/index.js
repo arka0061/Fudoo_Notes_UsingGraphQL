2e10505f4b4c339c41810401157253306d46af97
Arka <arka.spectacular@gmail.com>
2021-10-15 00:32:21 +0530

    feat:add label and delete label api implemented

--- Fundoo_Notes_UsingGraphQL/app/graphql/resolvers/index.js
+++ Fundoo_Notes_UsingGraphQL/app/graphql/resolvers/index.js
@@ -1,11 +1,9 @@
 // const resolvers = {
 //   ...userResolvers
 // }
-
-
-
 // module.exports = resolvers;
 module.exports = [
   // eslint-disable-next-line no-multi-assign
   userResolvers = require('./userResolvers'),
-  noteResolvers=require('./noteResolvers')];
+  noteResolvers=require('./noteResolvers'),
+  labelResolvers=require('./labelResolver')];
