76a625e6bded82ad3c18c946078b4bd49a6d14cb
Arka <arka.spectacular@gmail.com>
2021-10-25 14:58:58 +0530

    refactor:passport setup file refactored

--- Fundoo_Notes_UsingGraphQL/app/utilities/socialAuthentication/passport-setup.js
+++ Fundoo_Notes_UsingGraphQL/app/utilities/socialAuthentication/passport-setup.js
@@ -1,38 +1,23 @@
 const passport = require('passport');
 const userModel = require('../../models/user.model');
-const jwt=require('../jwtToken');
+const jwt = require('../jwtToken');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+require('dotenv').config();
 
-passport.serializeUser(function(user, done) {
-    /*
-    From the user take just the id (to minimize the cookie size) and just pass the id of the user
-    to the done callback
-    PS: You dont have to do it like this its just usually done like this
-    */
-    done(null, user);
-  });
-  
-passport.deserializeUser(function(user, done) {
-    /*
-    Instead of user this function usually recives the id 
-    then you use the id to select the user from the db and pass the user obj to the done callback
-    PS: You can later access this data in any routes in: req.user
-    */
-    done(null, user);
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
 });
 
 passport.use(new GoogleStrategy({
-    clientID: "1046536193804-54q92aoec85t2gekaj61hekcsc8to8sf.apps.googleusercontent.com",
-    clientSecret: "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",
-    callbackURL: "https://localhost:3000/google/callback"
-  },
- async function (accessToken, refreshToken, profile, done) {
-    /*
-     use the profile info (mainly profile id) to check if the user is registerd in ur db
-     If yes select the user and pass him to the done callback
-     If not create the user and then select him and pass to callback
-    */
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: "https://localhost:3000/google/callback"
+},
+  async function (accessToken, refreshToken, profile, done) {
     return done(null, profile);
-    
   }
 ));
